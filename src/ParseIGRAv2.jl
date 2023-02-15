d3800b5bf22b34146c304944570d2ac98f26f960
Nathanael Wong <natgeo.wong@outlook.com>
2023-02-15 17:18:42 -0500

    Just the basic stuff first before diving into it

--- src/ParseIGRAv2.jl
+++ src/ParseIGRAv2.jl
@@ -1,5 +1,26 @@
 module ParseIGRAv2
 
-# Write your package code here.
+## Base Modules Used
+using DelimitedFiles
+using Logging
+using Printf
+using Statistics
+
+## Modules Used
+using NCDatasets
+
+## Reexporting exported functions within these modules
+using Reexport
+@reexport using Dates
+
+import Base: download
+
+## Exporting the following functions:
+export
+        something
+
+## TmPi.jl logging preface
+
+modulelog() = "$(now()) - ParseIGRAv2.jl"
 
 end
