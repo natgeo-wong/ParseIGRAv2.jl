c87079cf455231457fd834eabc7de1a784eb6425
Nathanael Wong <natgeo.wong@outlook.com>
2023-04-06 00:54:31 -0400

    Not all stations have data either lol

--- src/filesystem.jl
+++ src/filesystem.jl
@@ -1,5 +1,5 @@
 zippath(station::IGRAv2StationRaw,path) = joinpath(path,"IGRAv2","raw","$(station.ID).txt.zip")
-txtpath(station::IGRAv2StationRaw,path) = joinpath(path,"IGRAv2","raw","$(station.ID).txt")
+txtpath(station::IGRAv2StationRaw,path) = joinpath(path,"IGRAv2","raw","$(station.ID)-data.txt")
 
 zippath(station::IGRAv2StationDerived,path) = joinpath(path,"IGRAv2","derived","$(station.ID).txt.zip")
 txtpath(station::IGRAv2StationDerived,path) = joinpath(path,"IGRAv2","derived","$(station.ID).txt")
